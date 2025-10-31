233a6150e0c7c7ad692fba4faa0d4bbcd70b5ba0
Oki Sulton <os.alimie@gmail.com>
2025-10-31 16:53:57 +0700

    fix: remove unnecessary React import and ensure URL polyfill is included

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,10 @@ import { PreferencesContext } from "@/hooks/preferences-store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import 'react-native-url-polyfill/auto';
 
 SplashScreen.preventAutoHideAsync();
 
